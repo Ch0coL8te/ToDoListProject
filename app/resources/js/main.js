5117ac1c8d883756dcc17d8e02685e809e0bc063
Ch0coL8te <ch0col8te@me.com>
2018-02-16 00:52:07 +0300

    some shit happens

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -9,35 +9,48 @@ let removeSVG =
   '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"> <g> <g> <path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3 c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9 C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7 c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2 c0.6,0,1.1,0.5,1.1,1.1V7z"/> </g> <g> <g> <path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/> </g> <g> <path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/> </g> <g> <path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8 C14.6,17.7,14.3,18,14,18z"/> </g> </g> </g> </svg>';
 let completeSVG =
   '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"> <rect y="0" class="noFill" width="22" height="22"/> <g> <path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8 c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/> </g> </svg>';
+
+  renderTodoList();  
+
 // user clicked on the add button
 // If there is any text inside item field, add that text to TODO list
 document.getElementById('add').addEventListener('click', function () {
   let value = document.getElementById('item').value;
   // If value exist, then do something
   if (value) {
-    addItemTodo(value);
-    document.getElementById("item").value = '';
-    // When a new element added - pushes it to data object's todo array
-    data.todo.push(value);    
-    dataObjectUpdated();
+    addItem(value);
   }
 });
 
-renderTodoList();
+document.getElementById('item').addEventListener('keydown', function(e) {
+  let value = this.value;
+  if (e.code === 'Enter' && value) {
+    addItem(value);
+  }
+});
+
+function addItem(value) {
+  addItemToDOM(value);
+  document.getElementById("item").value = "";
+  // When a new element added - pushes it to data object's todo array
+  data.todo.push(value);
+  dataObjectUpdated();
+}
 
 function renderTodoList() {
+  // If no data in 'data' object, then just return
   if (!data.todo.length && !data.todo.completed.length) {
     return;
   }
-
+  // Looping through todo list
   for (let i = 0; i < data.todo.length; i++) {
     let value = data.todo[i];
-    addItemTodo(value);
+    addItemToDOM(value);
   }
-
+  // Looping through completed list
   for (let j = 0; j < data.completed.length; j++) {
     let value = data.completed[j];
-    addItemTodo(value, true);
+    addItemToDOM(value, true);
   }
 }
 
@@ -92,7 +105,7 @@ function completeItem() {
 }
 
 // Add a new item to the todo list
-function addItemTodo(text, completed) {
+function addItemToDOM(text, completed) {
   let list = (completed) ? document.getElementById('completed') : document.getElementById('todo');
 
   let item = document.createElement('li');
@@ -122,3 +135,33 @@ function addItemTodo(text, completed) {
   // insert a list item before any other child li's
   list.insertBefore(item, list.childNodes[0]);
 }
+
+// function createClearLink() {
+//   let ul = document.getElementById('completed');
+
+//   let div = document.createElement('div');
+//   div.classList.add('clearLink');
+//   ul.parentNode.appendChild(div);
+
+//   let link = document.createElement('a');
+//   link.setAttribute('href', '#');
+//   link.innerText = 'Clear completed list';
+//   div.appendChild(link);
+// }
+// createClearLink();
+
+// function clearCompletedList() {
+//   let link = document.querySelector('.clearLink a');
+
+//   link.addEventListener('click', function() {
+//     if (confirm('Are you sure?')) {
+//       data.completed = [];
+//       dataObjectUpdated();
+//       for (let j = 0; j < data.completed.length; j++) {
+//         let value = data.completed[j];
+//         addItemToDOM(value, true);
+//       }
+//     }
+//   });
+// }
+// clearCompletedList();
